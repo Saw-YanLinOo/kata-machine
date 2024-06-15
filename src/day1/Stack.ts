9724bf11675607390b461d9aaf5a6d5ab32cd4ae
yan yan <yly80103@gmail.com>
2024-06-16 00:49:46 +0700

    implement Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,51 @@
+type Node<T> ={
+    vale:T,
+    prev?:Node<T>,
+}
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head? : Node<T>
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        this.length ++;
+        
+        const node =  {vale: item} as Node<T>;
+
+        if(!this.head){
+            this.head = node;
+            return;
+        }
+
+        node.prev = this.head;
+        this.head = node;
 
 }
     pop(): T | undefined {
 
+        this.length = Math.max(0,this.length-1);
+        if(this.length ===0){
+
+            const head = this.head;
+            this.head = undefined;
+
+            return head?.vale;
+        }
+
+       const head = this.head;
+       this.head = head?.prev;
+
+       return head?.vale;
+
 }
     peek(): T | undefined {
+
+        return this.head?.vale;
 
 }
 }
