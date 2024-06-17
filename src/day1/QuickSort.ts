60feb9355d6ba839e1df2c2a9ff04b2b34d77d5e
yan yan <yly80103@gmail.com>
2024-06-17 14:05:39 +0700

    implement quick sort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,37 @@
+function partation(arr:number[],lo:number,hi:number) :number{
+
+    const pivot = arr[hi];
+
+    let idx = lo-1;
+    for(let i=lo;i<hi;++i){
+        if(arr[i] <= pivot){
+            idx++;
+            const tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+    }
+
+    idx++;
+    arr[hi] = arr[idx];
+    arr[idx] = pivot;
+
+    return idx;
+    
+}
+
+function qs(arr:number[],lo:number,hi:number){
+
+    if(lo>=hi){
+        return;
+    }
+    const pivot = partation(arr,lo,hi);
+
+    qs(arr,lo,pivot-1);
+    qs(arr,pivot+1,hi);
+}
+
 export default function quick_sort(arr: number[]): void {
 
+    qs(arr,0,arr.length-1);
 }
