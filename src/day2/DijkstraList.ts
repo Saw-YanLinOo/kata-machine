f98190cefbe1a905d52cab934779b41a85ce7a09
yan yan <yly80103@gmail.com>
2024-06-25 03:03:09 +0700

    add DijKstra and LRU

---
+++ src/day2/DijkstraList.ts
@@ -0,0 +1,148 @@
+
+function hasUnVisited(seen:boolean[],dists:number[]):boolean{
+    return seen.some((s,i) => !s && dists[i]<Infinity);
+}
+
+
+function getLowestUnvisited(seen:boolean[],dists:number[]){
+    
+    let idx = -1;
+    let lowestDistance = Infinity;
+
+
+    for(let i = 0;i<seen.length;++i){
+
+        if(seen[i]){
+            continue;
+        }
+
+        if(dists[i]<lowestDistance){
+
+            lowestDistance = dists[i];
+            idx = i;
+        }
+    }
+
+
+    return idx;
+}
+
+
+
+
+function hasUnVisiteds(seen:boolean[],dists:number[]):boolean{
+
+    return seen.some((s,i) => !s && dists[i]<Infinity);
+}
+
+function getLowestUnvisiteds(seen:boolean[],dists:number[]): number{
+
+    let idx = -1;
+    let lowestDistance = Infinity;
+
+    for(let i=0;i<seen.length;++i){
+
+        if(seen[i]){
+            continue;
+        }
+
+        if(dists[i] < lowestDistance){
+            lowestDistance = dists[i];
+            idx =i;
+        }
+    }
+
+    return idx;
+}
+
+export default function dijkstra_list(
+    source: number,
+    sink: number,
+    arr: WeightedAdjacencyList): number[] {
+
+        const seen = new Array(arr.length).fill(false);
+        const prev = new Array(arr.length).fill(-1);
+        const dists = new Array(arr.length).fill(Infinity);
+
+
+        dists[source] = 0;
+
+        while(hasUnVisited(seen,dists)){
+
+            const curr = getLowestUnvisited(seen,dists);
+
+            seen[curr]=true;
+
+            const adjs = arr[curr];
+            for(let i=0;i<adjs.length;++i){
+
+                const edge = adjs[i];
+                if(seen[edge.to]){
+                    continue;
+                }
+
+                const dist = dists[curr]+edge.weight;
+                if(dist<dists[edge.to]){
+                    dists[edge.to] = dist;
+                    prev[edge.to] = curr;
+                }
+            }
+        }
+
+        let curr = sink;
+        let out:number[] = [];
+
+        while(prev[curr] !== -1){
+            out.push(curr);
+            curr = prev[curr];
+        }
+
+
+        out.push(source);
+        return out.reverse();
+
+
+
+
+
+
+        // const seen = new Array(arr.length).fill(false);
+        // const prev = new Array(arr.length).fill(-1);
+        // const dists = new Array(arr.length).fill(Infinity);
+
+        // dists[source] = 0;
+
+        // while(hasUnVisited(seen,dists)){
+
+        //     const curr = getLowestUnvisited(seen,dists);
+            
+        //     seen[curr] = true;
+
+        //     const adjs = arr[curr];
+        //     for(let i = 0; i<adjs.length; ++i){
+
+        //         const edge = adjs[i];
+        //         if(seen[edge.to]){
+        //             continue;
+        //         }
+
+        //         const dist = dists[curr] + edge.weight;
+        //         if(dist < dists[edge.to]){
+        //             dists[edge.to] = dist;
+        //             prev[edge.to] = curr;
+        //         }
+        //     }
+            
+        // }
+
+        // const out: number[] = [];
+        // let curr = sink;
+
+        // while(prev[curr] !== -1){
+        //     out.push(curr);
+        //     curr = prev[curr];
+        // }
+
+        // out.push(source);
+        // return out.reverse();
+}
